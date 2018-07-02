4d97cd78d76edb825a63bb2685a4cad86c20f355
vlasnikita <vlasenkonikita@gmail.com>
2018-07-02 21:13:03 +0300

    updates

--- script.js
+++ script.js
@@ -2,39 +2,46 @@ const header = document.querySelector('.header')
 const headerWrapper = document.querySelector('.header__wrapper')
 
 const hamburger = document.querySelector('.header__hamburger-container')
-const logo = document.querySelector('.header__logo')
+const logo = document.querySelector('.logo')
 const menu = document.querySelector('.menu')
-const menuItems = document.querySelectorAll('.menu__list-item')
+const menuItems = document.querySelectorAll('.menu-item a')
 
 window.addEventListener('scroll', e => {
-	if(window.scrollY > headerWrapper.offsetHeight) {
-		headerWrapper.style.height = '80px'
+	console.log(menuItems)
+	if(window.scrollY > 330) {
+		// headerWrapper.style.height = '80px'
 		
-		logo.style.height = '100px'
-		logo.style.fontSize = '4rem'
+		// logo.style.height = '50px'
+		// logo.style.fontSize = '4rem'
+		menu.style.backgroundColor = '#fff'
+		menuItems.forEach(item => item.style.color = '#223e4a')
+		// menu.style.width = '0vw'
+		// menu.style.height = '0px'
+		// menu.style.marginLeft = '-100vw'
+		// menu.style.opacity = '0'
+		// menu.style.borderTop = '1px solid rgba(34, 62, 74, 0)'
 
-		menu.style.width = '0vw'
-		menu.style.height = '0px'
-		menu.style.marginLeft = '-100vw'
-		menu.style.opacity = '0'
-
-		hamburger.style.marginLeft = '10px'
-		hamburger.style.opacity = '1'
-		hamburger.style.transition = 'all .5s 1.1s'	
-	} else if(window.scrollY <= headerWrapper.offsetHeight) {
-		headerWrapper.style.height = '25vh'
+		// hamburger.style.marginLeft = '10px'
+		// hamburger.style.opacity = '1'
+		// hamburger.style.transition = 'all .5s 1.1s'	
+	} 
+	else {
+		menu.style.backgroundColor = 'transparent'
+		menuItems.forEach(item => item.style.color = '#fff')
+	// 	headerWrapper.style.height = '30vh'
 		
-		logo.style.height = '200px'
-		logo.style.fontSize = '8rem'
+	// 	logo.style.height = '250px'
+	// 	logo.style.fontSize = '8rem'
 
-		menu.style.width = '100vw'
-		menu.style.height = '40px'
-		menu.style.marginLeft = '0'
-		menu.style.opacity = '1'
+	// 	menu.style.width = '100vw'
+	// 	menu.style.height = '40px'
+	// 	menu.style.marginLeft = '0'
+	// 	menu.style.opacity = '1'
+	// 	menu.style.borderTop = '1px solid rgba(34, 62, 74, .15)'
 
-		hamburger.style.marginLeft = '-10px'
-		hamburger.style.opacity = '0'
-		hamburger.style.transition = 'all .5s'
+	// 	hamburger.style.marginLeft = '-10px'
+	// 	hamburger.style.opacity = '0'
+	// 	hamburger.style.transition = 'all .5s'
 	}
 })
 
@@ -49,10 +56,15 @@ function openTab(evt, tabName) {
     // })
 
     card.style.transform = !card.style.transform ? 'rotateY(180deg)' : ''
+    // card.style.transform = !card.style.transform ? 'rotateY(180deg) translateX(-100%)' : ''
 
     tabLinks.forEach(link => { link.className = link.className.replace(' active', '') })
 
     evt.currentTarget.className = evt.currentTarget.className.includes('active') ?
     	evt.currentTarget.className : 
     	`${evt.currentTarget.className} active`
+}
+
+function focusInput() {
+	document.getElementById('name').focus();
 }

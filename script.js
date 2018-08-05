79a9e2b42bbf3fbf644c313b6709ee3221846e53
Nikita Vlasenko <vlasenkonikita@gmail.com>
2018-08-05 13:36:38 +0300

    Переработан блок видео на заставке, изменена структура услуг

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+let timeoutEnded = false
+
 document.addEventListener("DOMContentLoaded", function(event) { 
-    listenVideo();
-    setInterval(wrapperBoxShadowResize, 2000)
     listenScroll();
-    listenOffersArrow();
 });
 
 ymaps.ready(init);
@@ -16,40 +15,6 @@ ymaps.ready(init);
 
 ///////////////////////////////////////////////////////
 
-const listenVideo = () => {
-    const video = document.querySelector('.video');
-    const wrapper = document.querySelector('.helloMessage__wrapper');
-    const helloMessage = document.querySelector('.helloMessage');
-
-    document.querySelector('.video').addEventListener('mouseover', e => {
-        wrapper.style.left = '60%';
-        wrapper.style.opacity = '.5';
-
-        helloMessage.style.left = '60%';
-        helloMessage.style.opacity = '.5';
-    })
-
-    document.querySelector('.video').addEventListener('mouseleave', e => {
-        wrapper.style.left = '40%';
-        wrapper.style.opacity = '1';
-
-        helloMessage.style.left = '40%';
-        helloMessage.style.opacity = '1';
-    })
-}
-
-function wrapperBoxShadowResize (){
-    const wrapper = document.querySelector('.helloMessage__wrapper');
-    
-    if(this.spreaded){
-        this.spreaded = false
-        wrapper.style.boxShadow = '#666 -1px -1px 0px 0px';
-    } else {
-        this.spreaded = true
-        wrapper.style.boxShadow = '#666 -1px -1px 20px 0px';
-    }
-}
-
 const openMenu = () => {
     const menu = document.querySelector('.nav');
     menu.className += ' show';
@@ -61,51 +26,20 @@ const closeMenu = () => {
 }
 
 const listenScroll = () => {
-    window.addEventListener('scroll', () => {
-        const header = document.querySelector('.header');
-        const about = document.querySelector('.about');
+    window.addEventListener('scroll', (e) => {
+        const main = document.querySelector('.main');
 
-        if(window.scrollY >= about.scrollHeight / 6 * 5) {
-            about.style.opacity = '1';
-            header.style.color = '#000';
-            header.style.backgroundColor = '#fff';
-            header.style.boxShadow = '0 0 20px 1px #ddd';
+        if(window.scrollY > 0) {
+            if(!timeoutEnded){
+                window.scroll(0,1)
+                main.style.transform = 'translateY(-100%)'
+                setTimeout(() => {
+                    timeoutEnded = true
+                }, 850)
+            }
         } else {
-            about.style.opacity = '0';
-            header.style.color = '#fff';
-            header.style.backgroundColor = 'transparent';
-            header.style.boxShadow = '0 0 20px 1px transparent';
+            timeoutEnded = false
+            main.style.transform = 'translateY(-0%)'
         }
     })
-}
-
-const listenOffersArrow = () => {
-    const arrowRight = document.querySelector('.offers__arrow--right');
-    const arrowLeft = document.querySelector('.offers__arrow--left');
-    const offersBlock = document.querySelector('.offers__block');
-
-    arrowRight.addEventListener('click', () => {
-        offersBlock.style.transform = 'translate(-50%, -50%)';
-        arrowRight.className = 'offers__arrow';
-        arrowLeft.className = 'offers__arrow offers__arrow--left offers__arrow--active';
-    });
-    arrowLeft.addEventListener('click', () => {
-        offersBlock.style.transform = 'translate(0%, -50%)';
-        arrowLeft.className = 'offers__arrow offers__arrow--left';
-        arrowRight.className = 'offers__arrow offers__arrow--active';
-    });
-
-    arrowRight.addEventListener('mouseover', () => {
-        arrowRight.style.transform = 'translate(15%, -50%)';
-    });
-    arrowLeft.addEventListener('mouseover', () => {
-        arrowLeft.style.transform = 'translate(-15%, -50%)';
-    });
-
-    arrowRight.addEventListener('mouseleave', () => {
-        arrowRight.style.transform = 'translate(0%, -50%)';
-    });
-    arrowLeft.addEventListener('mouseleave', () => {
-        arrowLeft.style.transform = 'translate(0%, -50%)';
-    });
 }

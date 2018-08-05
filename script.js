d70df2b53fe45c2439ed27c16cd5d4de5eb775b7
Nikita Vlasenko <vlasenkonikita@gmail.com>
2018-08-05 23:36:54 +0300

    Доделан тогглер цветовой гаммы

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 ymaps.ready(init);
 
-        function init () {
-            var myMap = new ymaps.Map('map', {
-                    center: [55.749750, 37.542581],
-                    zoom: 16
-                });
-        }
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.749750, 37.542581],
+            zoom: 16
+        });
+}
 
 ///////////////////////////////////////////////////////
 
@@ -44,182 +44,223 @@ const listenScroll = () => {
     })
 }
 
-// Тестируем цвета
 const button1 = document.querySelector('.color__button1');
 const button2 = document.querySelector('.color__button2');
 const button3 = document.querySelector('.color__button3');
 
-//colors:
-const navItem = document.querySelector('.nav__item');
-const header = document.querySelector('.header');
-const headerEmail = document.querySelector('.header__email');
-const button = document.querySelector('.button');
-const hellloMessageTitle = document.querySelector('.helloMessage__title');
-const hellloMessageText = document.querySelector('.helloMessage__text');
-const cardTitle = document.querySelector('.card__title');
-const cardText = document.querySelector('.card__text');
-const cardTextAdvertisers = document.querySelector('.card__text--advertisers');
-const cardTextStreamers = document.querySelector('.card__text--streamers');
-const backgroundTitleAdvertisers = document.querySelector('.background__title--advertisers');
-const backgroundTitleStreamers = document.querySelector('.background__title--streamers');
-const cardAside = document.querySelector('.card__aside');
-const cardItem = document.querySelector('.card__item');
-const backgroundTitle = document.querySelector('.background__title');
-const contactsContact = document.querySelector('.contacts__contact');
-const contactsInput = document.querySelector('.contacts__input');
-
-//background-colors:
-const headerMenu = document.querySelector('.header__menu span');
-const main = document.querySelector('.main');
-const about = document.querySelector('.about');
-const contactsBlock = document.querySelector('.contacts__block');
-const contactsBlockForm = document.querySelector('.contacts__block--form');
-const advertisers = document.querySelector('.advertisers');
-const streamers = document.querySelector('.streamers');
-
-//Яркие цвета для элементов
-const nav = document.querySelector('.nav');
-const buttonCta = document.querySelector('.button--cta');
-const buttonInfo = document.querySelector('.button--info');
-const buttonSubmit = document.querySelector('.button--submit');
-
-
 button1.addEventListener('click', e => {
-    //текст
-    navItem.style.color = '#000';
-    header.style.color = '#000';
-    headerEmail.style.color = '#000';
-    button.style.color = '#000';
-    hellloMessageTitle.style.color = '#000';
-    hellloMessageText.style.color = '#000';
-    cardTitle.style.color = '#000';
-    cardText.style.color = '#000';
-    cardTextAdvertisers.style.color = '#000';
-    cardTextStreamers.style.color = '#000';
-    backgroundTitleAdvertisers.style.color = '#000';
-    backgroundTitleStreamers.style.color = '#000';
-    cardAside.style.color = '#000';
-    cardItem.style.color = '#000';
-    backgroundTitle.style.color = '#000';
-    contactsContact.style.color = '#000';
-    contactsInput.style.color = '#000';
 
-    //фон
-    header.style.backgroundColor = '#fff';
-    headerMenu.style.backgroundColor = '#fff';
-    main.style.backgroundColor = '#fff';
-    about.style.backgroundColor = '#fff'
-    contactsBlock.style.backgroundColor = '#fff';
-    contactsBlockForm.style.backgroundColor = '#fff';
-    advertisers.style.backgroundColor = '#fff';
-    streamers.style.backgroundColor = '#fff';
+    const colors = [
+        ...document.querySelectorAll('.colorPanel-blue-color'),
+        ...document.querySelectorAll('.colorPanel-orange-color')
+    ]
 
-    //специальные элементы
-    nav.style.backgroundColor = '#b91313';
-    buttonCta.style.backgroundColor = '#b91313';
-    buttonCta.style.color = '#000';
-    buttonInfo.style.border = '2px solid #b91313;';
-    buttonInfo.style.backgroundColor = '#b91313;';
-    buttonInfo.style.color = '#000;';
-    buttonSubmit.style.backgroundColor = '#b91313';
-    buttonSubmit.style.color = '#000';
+    const backgrounds = [
+        ...document.querySelectorAll('.colorPanel-blue-backgroundColor'),
+        ...document.querySelectorAll('.colorPanel-orange-backgroundColor')
+    ]
 
-    console.log("BUTTON-1 was clicked");
+    const borders = [
+        ...document.querySelectorAll('.colorPanel-blue-border'),
+        ...document.querySelectorAll('.colorPanel-orange-border')
+    ]
+
+    const shadows = [
+        ...document.querySelectorAll('.colorPanel-blue-boxShadow'),
+        ...document.querySelectorAll('.colorPanel-orange-boxShadow')
+    ]
+
+    const shadowsInfo = [
+        ...document.querySelectorAll('.colorPanel-blue-boxShadowInfo'),
+        ...document.querySelectorAll('.colorPanel-orange-boxShadowInfo')
+    ]
+
+    const specials = [
+        ...document.querySelectorAll('.colorPanel-blue-backgroundColorSpecial'),
+        ...document.querySelectorAll('.colorPanel-orange-backgroundColorSpecial')
+    ]
+
+    const afters = [
+        ...document.querySelectorAll('.colorPanel-blue-backgroundColorAfter'),
+        ...document.querySelectorAll('.colorPanel-orange-backgroundColorAfter')
+    ]
+
+    colors.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-color', 'colorPanel-red-color')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-color', 'colorPanel-red-color')
+    })
+
+    backgrounds.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-backgroundColor', 'colorPanel-red-backgroundColor')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-backgroundColor', 'colorPanel-red-backgroundColor')
+    })
+
+    borders.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-border', 'colorPanel-red-border')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-border', 'colorPanel-red-border')
+    })
+
+    shadows.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-shadows', 'colorPanel-red-shadows')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-shadows', 'colorPanel-red-shadows')
+    })
+
+    shadowsInfo.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-boxShadowInfo', 'colorPanel-red-boxShadowInfo')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-boxShadowInfo', 'colorPanel-red-boxShadowInfo')
+    })
+
+    specials.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-backgroundColorSpecial', 'colorPanel-red-backgroundColorSpecial')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-backgroundColorSpecial', 'colorPanel-red-backgroundColorSpecial')
+    })
+
+    afters.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-backgroundColorAfter', 'colorPanel-red-backgroundColorAfter')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-backgroundColorAfter', 'colorPanel-red-backgroundColorAfter')
+    })
 })
 
 button2.addEventListener('click', e => {
-    //текст
-    navItem.style.color = '#000';
-    header.style.color = '#000';
-    headerEmail.style.color = '#000';
-    button.style.color = '#000';
-    hellloMessageTitle.style.color = '#000';
-    hellloMessageText.style.color = '#000';
-    cardTitle.style.color = '#000';
-    cardText.style.color = '#000';
-    cardTextAdvertisers.style.color = '#000';
-    cardTextStreamers.style.color = '#000';
-    backgroundTitleAdvertisers.style.color = '#000';
-    backgroundTitleStreamers.style.color = '#000';
-    cardAside.style.color = '#000';
-    cardItem.style.color = '#000';
-    backgroundTitle.style.color = '#000';
-    contactsContact.style.color = '#000';
-    contactsInput.style.color = '#000';
+    const colors = [
+        ...document.querySelectorAll('.colorPanel-red-color'),
+        ...document.querySelectorAll('.colorPanel-orange-color')
+    ]
 
-    //фон
-    header.style.backgroundColor = '#fff';
-    headerMenu.style.backgroundColor = '#fff';
-    main.style.backgroundColor = '#fff';
-    about.style.backgroundColor = '#fff'
-    contactsBlock.style.backgroundColor = '#fff';
-    contactsBlockForm.style.backgroundColor = '#fff';
-    advertisers.style.backgroundColor = '#fff';
-    streamers.style.backgroundColor = '#fff';
+    const backgrounds = [
+        ...document.querySelectorAll('.colorPanel-red-backgroundColor'),
+        ...document.querySelectorAll('.colorPanel-orange-backgroundColor')
+    ]
 
-    //специальные элементы
-    nav.style.backgroundColor = '#1d1d7f';
-    buttonCta.style.backgroundColor = '#1d1d7f';
-    buttonInfo.style.border = '2px solid #1d1d7f';
-    buttonInfo.style.backgroundColor = '#1d1d7f';
-    buttonSubmit.style.backgroundColor = '#1d1d7f';
+    const borders = [
+        ...document.querySelectorAll('.colorPanel-red-border'),
+        ...document.querySelectorAll('.colorPanel-orange-border')
+    ]
 
-    console.log("BUTTON-2 was clicked");
+    const shadows = [
+        ...document.querySelectorAll('.colorPanel-red-boxShadow'),
+        ...document.querySelectorAll('.colorPanel-orange-boxShadow')
+    ]
+
+    const shadowsInfo = [
+        ...document.querySelectorAll('.colorPanel-red-boxShadowInfo'),
+        ...document.querySelectorAll('.colorPanel-orange-boxShadowInfo')
+    ]
+
+    const specials = [
+        ...document.querySelectorAll('.colorPanel-red-backgroundColorSpecial'),
+        ...document.querySelectorAll('.colorPanel-orange-backgroundColorSpecial')
+    ]
+
+    const afters = [
+        ...document.querySelectorAll('.colorPanel-red-backgroundColorAfter'),
+        ...document.querySelectorAll('.colorPanel-orange-backgroundColorAfter')
+    ]
+
+    colors.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-red-color', 'colorPanel-blue-color')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-color', 'colorPanel-blue-color')
+    })
+
+    backgrounds.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-red-backgroundColor', 'colorPanel-blue-backgroundColor')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-backgroundColor', 'colorPanel-blue-backgroundColor')
+    })
+
+    borders.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-red-border', 'colorPanel-blue-border')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-border', 'colorPanel-blue-border')
+    })
+
+    shadows.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-red-boxShadow', 'colorPanel-blue-boxShadow')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-boxShadow', 'colorPanel-blue-boxShadow')
+    })
+
+    shadowsInfo.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-red-boxShadowInfo', 'colorPanel-blue-boxShadowInfo')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-boxShadowInfo', 'colorPanel-blue-boxShadowInfo')
+    })
+
+    specials.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-red-backgroundColorSpecial', 'colorPanel-blue-backgroundColorSpecial')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-backgroundColorSpecial', 'colorPanel-blue-backgroundColorSpecial')
+    })
+
+    afters.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-red-backgroundColorAfter', 'colorPanel-blue-backgroundColorAfter')
+        el.classList.value = el.classList.value.replace('colorPanel-orange-backgroundColorAfter', 'colorPanel-blue-backgroundColorAfter')
+    })
 })
 
 button3.addEventListener('click', e => {
-    //текст
-    navItem.style.color = '#fff';
-    header.style.color = '#fff';
-    headerEmail.style.color = '#fff';
-    button.style.color = '#fff';
-    hellloMessageTitle.style.color = '#fff';
-    hellloMessageText.style.color = '#fff';
-    cardTitle.style.color = '#fff';
-    cardText.style.color = '#fff';
-    cardTextAdvertisers.style.color = '#fff';
-    cardTextStreamers.style.color = '#fff';
-    backgroundTitleAdvertisers.style.color = '#fff';
-    backgroundTitleStreamers.style.color = '#fff';
-    cardAside.style.color = '#fff';
-    cardItem.style.color = '#fff';
-    backgroundTitle.style.color = '#fff';
-    contactsContact.style.color = '#fff';
-    contactsInput.style.color = '#fff';
+    const colors = [
+        ...document.querySelectorAll('.colorPanel-blue-color'),
+        ...document.querySelectorAll('.colorPanel-red-color')
+    ]
 
-    //фон
-    header.style.backgroundColor = '#303030';
-    headerMenu.style.backgroundColor = '#303030';
-    main.style.backgroundColor = '#303030';
-    about.style.backgroundColor = '#303030'
-    contactsBlock.style.backgroundColor = '#303030';
-    contactsBlockForm.style.backgroundColor = '#303030';
-    advertisers.style.backgroundColor = '#303030';
-    streamers.style.backgroundColor = '#303030';
+    const backgrounds = [
+        ...document.querySelectorAll('.colorPanel-blue-backgroundColor'),
+        ...document.querySelectorAll('.colorPanel-red-backgroundColor')
+    ]
 
-    //специальные элементы
-    nav.style.backgroundColor = '#e08342';
-    buttonCta.style.backgroundColor = '#e08342';
-    buttonInfo.style.border = '2px solid #e08342';
-    buttonInfo.style.backgroundColor = '#e08342';
-    buttonSubmit.style.backgroundColor = '#e08342';
+    const borders = [
+        ...document.querySelectorAll('.colorPanel-blue-border'),
+        ...document.querySelectorAll('.colorPanel-red-border')
+    ]
 
-    console.log("BUTTON-3 was clicked");
+    const shadows = [
+        ...document.querySelectorAll('.colorPanel-blue-boxShadow'),
+        ...document.querySelectorAll('.colorPanel-red-boxShadow')
+    ]
+
+    const shadowsInfo = [
+        ...document.querySelectorAll('.colorPanel-blue-boxShadowInfo'),
+        ...document.querySelectorAll('.colorPanel-red-boxShadowInfo')
+    ]
+
+    const specials = [
+        ...document.querySelectorAll('.colorPanel-blue-backgroundColorSpecial'),
+        ...document.querySelectorAll('.colorPanel-red-backgroundColorSpecial')
+    ]
+
+    const afters = [
+        ...document.querySelectorAll('.colorPanel-blue-backgroundColorAfter'),
+        ...document.querySelectorAll('.colorPanel-red-backgroundColorAfter')
+    ]
+
+    colors.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-color', 'colorPanel-orange-color')
+        el.classList.value = el.classList.value.replace('colorPanel-red-color', 'colorPanel-orange-color')
+    })
+
+    backgrounds.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-backgroundColor', 'colorPanel-orange-backgroundColor')
+        el.classList.value = el.classList.value.replace('colorPanel-red-backgroundColor', 'colorPanel-orange-backgroundColor')
+    })
+
+    borders.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-border', 'colorPanel-orange-border')
+        el.classList.value = el.classList.value.replace('colorPanel-red-border', 'colorPanel-orange-border')
+    })
+
+    shadows.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-boxShadow', 'colorPanel-orange-boxShadow')
+        el.classList.value = el.classList.value.replace('colorPanel-red-boxShadow', 'colorPanel-orange-boxShadow')
+    })
+
+    shadowsInfo.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-boxShadowInfo', 'colorPanel-orange-boxShadowInfo')
+        el.classList.value = el.classList.value.replace('colorPanel-red-boxShadowInfo', 'colorPanel-orange-boxShadowInfo')
+    })
+
+    specials.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-backgroundColorSpecial', 'colorPanel-orange-backgroundColorSpecial')
+        el.classList.value = el.classList.value.replace('colorPanel-red-backgroundColorSpecial', 'colorPanel-orange-backgroundColorSpecial')
+    })
+
+    afters.forEach(el => {
+        el.classList.value = el.classList.value.replace('colorPanel-blue-backgroundColorAfter', 'colorPanel-orange-backgroundColorAfter')
+        el.classList.value = el.classList.value.replace('colorPanel-red-backgroundColorAfter', 'colorPanel-orange-backgroundColorAfter')
+    })
 })
-
-//TO DO addEventListener on ".card__aside:before"(color: #b91313;)
-
-// 1)
-// фон #fff
-// текст #000
-// яркий цвет для элементов #b91313
-
-// 2)
-// фон #fff
-// текст #000
-// яркий цвет для элементов #1d1d7f
-
-// 3)
-// фон #303030
-// текст #fff
-// яркий цвет для элементов #e08342

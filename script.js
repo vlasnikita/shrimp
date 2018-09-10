6b176377d14b9216c9a0bf502a8da8973dc65561
Nikita <vlasenkonikita@gmail.com>
2018-09-10 11:52:11 +0300

    Апдейт с цветов разделением по блокам

--- script.js
+++ script.js
@@ -1,41 +1,73 @@
-let timeoutEnded = false
+window.requestAnimationFrame = window.requestAnimationFrame
+ || window.mozRequestAnimationFrame
+ || window.webkitRequestAnimationFrame
+ || window.msRequestAnimationFrame
+ || function(f){setTimeout(f, 1000/60)}
 
+ // PARALLAX
+const parallax = document.querySelector('.parallax');
+const enableParallax = () => {
+    const scrolltop = window.pageYOffset; // get number of pixels document has scrolled vertically 
+    parallax.style.top = -scrolltop * 1.25 + 'px';
+}
+window.addEventListener('scroll', function(){
+    requestAnimationFrame(enableParallax)
+   }, false)
+
+// Анимация заставки
+let currentImg = 0;
+const images = parallax.querySelectorAll('img')
+setInterval(() => {
+    let temp;
+    images.forEach((img, i) => {
+        if (i === currentImg + 1){
+            img.className = 'parallax__visible';
+            temp = currentImg + 1;
+        }else{
+            img.className = '';
+        }
+    })
+    if(!temp){
+        images[0].className = 'parallax__visible';
+        currentImg = 0;
+    } else {
+        currentImg = temp;
+    }
+}, 3500)
+
+// Карточки с услугами
+const briefs = document.querySelectorAll('.service__brief');
+window.addEventListener('click', function(e){
+    briefs.forEach(brief => {
+        console.log(0)
+        if(brief === e.target || brief.contains(e.target)){
+            brief.className = 'service__brief service__brief--active';
+        } else {
+            brief.className = 'service__brief';
+        }
+    })
+}, false)
+
+// Меню
 const openMenu = () => {
     const menu = document.querySelector('.nav');
     menu.className += ' show';
 }
-
 const closeMenu = () => {
     const menu = document.querySelector('.nav');
     menu.className = ' nav';
 }
 
-const listenScroll = () => {
-    window.addEventListener('scroll', (e) => {
-        const main = document.querySelector('.main');
-
-        if(window.scrollY > 0) {
-            if(!timeoutEnded){
-                window.scroll(0,1)
-                main.style.transform = 'translateY(-100%)'
-                setTimeout(() => {
-                    timeoutEnded = true
-                }, 850)
-            }
-        } else {
-            timeoutEnded = false
-            main.style.transform = 'translateY(-0%)'
-        }
-    })
-}
-
+// Перевод фокуса в различные места сайта
 const focusForm = () => {
     document.querySelector('#name').scrollIntoView({ block: 'start',  behavior: 'smooth' })
     setTimeout(() => {
         document.querySelector('#name').focus();
     }, 500);
 };
-
+const focusBlock = (name) => {
+    document.querySelector('#' + name).scrollIntoView({ block: 'start',  behavior: 'smooth' });
+}
 const focusFormFromVideo = () => {
         document.querySelector('#name').focus();
     setTimeout(() => {
@@ -43,10 +75,7 @@ const focusFormFromVideo = () => {
     }, 1200);
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    listenScroll();
-});
-
+// Карта
 ymaps.ready(init);
 
 function init () {

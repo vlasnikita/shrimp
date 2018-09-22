65a3de9018008269601411cc500df88d93da451f
Nikita Vlasenko <vlasnikita@yandex-team.ru>
2018-09-22 19:37:56 +0300

    Фиксы + сборка под разные страницы

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function indfox_setup() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_script( 'my_script', get_template_directory_uri() . '/script.js', array(), '1.0');
+	wp_enqueue_script( 'my_script', get_template_directory_uri() . '/index.js', array(), '1.0');
 }
 
 function features(){

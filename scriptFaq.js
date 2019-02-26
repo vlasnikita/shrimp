a1dfae564da560d51e904d5bda5f90793b1527a9
Nikita <vlasenkonikita@gmail.com>
2019-02-26 09:55:03 +0300

    d

--- scriptFaq.js
+++ scriptFaq.js
@@ -1,14 +1,18 @@
 const answers = document.querySelectorAll('.answer')
 
 function toggleAnswer(e){
+    console.log(e.target)
     for(let i=0; i<answers.length; i++){
         if(answers[i] === e.target || answers[i].contains(e.target)){
             if(answers[i].className.includes('active')){
+                console.log('includes')
                 answers[i].className = 'answer'
             } else {
+                console.log('not includes')
                 answers[i].className = 'answer active'
             }
         } else {
+            console.log('not clicked')
             answers[i].className = 'answer'
         }
     }
